30df1ff1a4f252ec5277e5d365b1c2940fbe7961
99ShinByeongseon <sun6988shin@naver.com>
2025-02-14 06:28:23 +0900

    feat: TodoProvider 추가 및 샘플 할 일 데이터 상수화, TodoDetailPage에서 할 일 상세 정보 표시

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
-import TodoProvider from "../components/provider/TodoProvider"
 import TodoContainer from "../components/provider/TodoProvider"
 
 const HomePage = () => {
-    return (
-        <TodoProvider>
-        <TodoContainer />
-        </TodoProvider>
-    );
+    return <TodoContainer />;
 };
 
 export default HomePage;
