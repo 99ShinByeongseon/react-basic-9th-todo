5d09fb25fed9b7197ed911b349b36ed503f53209
99ShinByeongseon <sun6988shin@naver.com>
2025-02-21 07:26:51 +0900

    feat: TodoProvider에서 API 연동 추가 및 할 일 목록 자동 갱신 기능 구현

--- src/components/provider/TodoProvider.jsx
+++ src/components/provider/TodoProvider.jsx
@@ -1,32 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoContext } from "../../context/TodoContext";
-import { SAMPLE_TODOS } from "../../constants/sample-todos";
+import { todoClient } from "../../lib/todoClient";
 
 const TodoProvider = ({ children }) => {
-  const [todos, setTodos] = useState(SAMPLE_TODOS);
+  const [todos, setTodos] = useState([]);
 
-  const addTodos = (text) => {
-    setTodos([{ id: crypto.randomUUID(), text, completed: false }, ...todos]);
+  const getTodos = async () => {
+    const { data } = await todoClient.get("/");
+    
+    setTodos(data);
+  }
+
+  const addTodos = async (text) => {
+    const { data } = await todoClient.post("/", {
+      text,
+      completed: false,
+    });
+
+    await getTodos();
+
+    return data;
   };
 
-  const toggleTodoCompleted = (id) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              completed: !todo.completed,
-            }
-          : todo
-      )
-    );
+  const toggleTodoCompleted = async(id, currentCompleted) => {
+    const { data } = await todoClient.patch(`/${id}`, {
+      completed: !currentCompleted,
+    });
+
+    await getTodos();
+
+    return data;
   };
   // 삼항 연산자
   // 조건 ? 참일 때 : 거짓일 때
 
-  const deleteTodo = (id) => {
+  const deleteTodo = async(id) => {
     // todo.id가 내가 찾는 id와 같지 않을 때 true를 반환하여 그대로 남겨둠
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    const { data } = await todoClient.delete(`/${id}`);
+
+    await getTodos();
+
+    return data;
   };
 
   const getfilteredTodos = (selectedFilter) => {
@@ -40,6 +54,10 @@ const TodoProvider = ({ children }) => {
 
     return todos;
   };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
   <TodoContext.Provider
