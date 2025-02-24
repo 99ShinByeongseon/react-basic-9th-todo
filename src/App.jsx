5e551e9e592780c565ea1df2923b9f809e763ebf
99ShinByeongseon <sun6988shin@naver.com>
2025-02-24 11:25:15 +0900

    feat: React Query를 사용하기 위해 QueryProvider 추가 및 App 컴포넌트에서 TodoProvider를 QueryProvider로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./components/layout/RootLayout";
+import QueryProvider from "./components/provider/TodoProvider";
 import HomePage from "./pages/HomePage";
 import TodoDetailPage from "./pages/TodoDetailPage";
-import TodoProvider from "./components/provider/TodoProvider";
 
 const App = () => {
   return (
-    <TodoProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <QueryProvider>
         <Routes>
           <Route path="/" element={<RootLayout />}>
             <Route index element={<HomePage />} />
             <Route path="todos/:id" element={<TodoDetailPage />} />
           </Route>
         </Routes>
-      </BrowserRouter>
-    </TodoProvider>
+      </QueryProvider>
+    </BrowserRouter>
   );
 };
 
