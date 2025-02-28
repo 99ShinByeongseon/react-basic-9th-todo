c5cd9106b0b16b8c0bcd79368d39ddc5c3b23be4
99ShinByeongseon <sun6988shin@naver.com>
2025-02-28 12:35:51 +0900

    feat: 테마 전환 버튼 추가 및 테마 초기화 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./components/layout/RootLayout";
-import QueryProvider from "./components/provider/TodoProvider";
+import QueryProvider from "./components/provider/QueryProvider";
 import HomePage from "./pages/HomePage";
 import TodoDetailPage from "./pages/TodoDetailPage";
+import { useEffect } from "react";
+import { useThemeStore } from "./hooks/useThemeStore";
 
 const App = () => {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    // 현재 테마 초기화
+    root.classList.remove("dark", "light");
+
+    // 테마 설정
+    root.classList.add(theme);
+  }, [theme]);
+  
   return (
     <BrowserRouter>
       <QueryProvider>
--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import ThemeSwitchButton from "../ThemeSwitchButton";
 
 // Compositional Layout
 const RootLayout = () => {
@@ -7,6 +8,8 @@ const RootLayout = () => {
       <div className="max-w-3xl mx-auto p-4">
         <Outlet />
       </div>
+
+      <ThemeSwitchButton />
     </main>
   );
 };
