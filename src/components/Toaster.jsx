2f36ae8ded8f62d906e042ffe23db582c92817a7
99ShinByeongseon <sun6988shin@naver.com>
2025-03-04 07:34:10 +0900

    feat: Toaster 컴포넌트에 @formkit/auto-animate 추가 및 애니메이션 적용

--- src/components/Toaster.jsx
+++ src/components/Toaster.jsx
@@ -1,17 +1,25 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useToast } from "../hooks/useToast";
 
 const Toaster = () => {
-    const { toasts } = useToast();
+  const { toasts } = useToast();
+  const [parent] = useAutoAnimate();
 
-    return (
-        <ul className="fixed top-4 left-1/2 z-40 space-y-2 -translate-x-1/2">
-            {toasts.map((toast) => (
-                <li key={toast.id} className="bg-white rounded-md shadow px-4 py-2">
-                    👍 {toast.message}
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    // 부모로 css 이동동
+    <div className="fixed top-4 left-1/2 -translate-x-1/2 z-40 w-80">
+      <ul ref={parent} className="space-y-2">
+        {toasts.map((toast) => (
+          <li
+            key={toast.id}
+            className="bg-white rounded-md shadow px-4 py-2 w-full"
+          >
+            👍 {toast.message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Toaster;
