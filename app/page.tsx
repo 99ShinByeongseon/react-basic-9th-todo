a7d4bd43f979d894f131aca38cd69e0376162feb
99ShinByeongseon <sun6988shin@naver.com>
2025-03-14 08:14:12 +0900

    feat: 할 일 목록 가져오기 기능 추가 및 홈페이지 업데이트트

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import { getTodos } from "@/api/todo-api";
 import Link from "next/link";
 import React from "react";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const todos = await getTodos();
+
   return (
     <div>
       Homepage
       <Link href="/detial">Detail Page</Link>
+      <ul>
+        {todos.map(({ id, text, completed }) => (
+          <li key={id}>
+            {text} {completed ? "(Completed)" : "(Pending)"}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
