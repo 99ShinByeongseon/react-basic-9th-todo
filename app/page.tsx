099b1d56978e0fb4607957279523d230ab8ff83d
99ShinByeongseon <sun6988shin@naver.com>
2025-03-14 09:07:47 +0900

    feat: 페이지 및 컴포넌트 초기화

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import { getTodos } from "@/api/todo-api";
-import Link from "next/link";
 import React from "react";
 
-const Homepage = async () => {
-  const todos = await getTodos();
-
-  return (
-    <div>
-      Homepage
-      <Link href="/detial">Detail Page</Link>
-      <ul>
-        {todos.map(({ id, text, completed }) => (
-          <li key={id}>
-            {text} {completed ? "(Completed)" : "(Pending)"}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const HomePage = () => {
+  return <div>HomePage</div>;
 };
 
-export default Homepage;
+export default HomePage;
