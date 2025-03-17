82eb78fae7d0afe0b911edcc5ee419fdb9baf2f9
99ShinByeongseon <sun6988shin@naver.com>
2025-03-17 10:36:07 +0900

    feat: TodoHeader에 홈 링크 추가 및 레이아웃 수정

--- components/todo/layout/TodoHeader.tsx
+++ components/todo/layout/TodoHeader.tsx
@@ -1,10 +1,13 @@
+import { Link } from "lucide-react";
 import React from "react";
 
 const TodoHeader = () => {
   return (
     <header className="mb-4">
-      <div className="contianer mx-auto p-2">
+      <div className="contianer mx-auto p-2 flex-row">
+        <Link href="/">
         <h1 className="text-4xl font-bold">Todo List</h1>
+        </Link>
       </div>
     </header>
   );
