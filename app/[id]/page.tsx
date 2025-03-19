e6cbd4fe7895e1fa165a1e6329bfc821cbdd1624
99ShinByeongseon <sun6988shin@naver.com>
2025-03-19 15:29:51 +0900

    feat: React Query를 사용하여 Todo 상세 페이지 및 홈 페이지에서 데이터 사전 가져오기 기능 추가

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,8 +1,12 @@
 import { getTodoItem } from "@/api/todo-api";
-import TodoItem from "@/components/todo/TodoItem";
+import TodoDetail from "@/components/todo/TodoDetail";
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
-import React from "react";
+import { 
+  dehydrate, 
+  HydrationBoundary, 
+  QueryClient 
+} from "@tanstack/react-query";
+import Link from "next/link";
 
 interface DetailPageProps {
   params: Promise<{ id: string }>;
@@ -10,18 +14,25 @@ interface DetailPageProps {
 
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params;
-  const todoItem = await getTodoItem(id);
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos, id"],
+    queryFn: () => getTodoItem(id),
+  });
 
   return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
     <section>
       <div className="container p-2 mx-auto space-y-4">
-        <TodoItem todo={todoItem} />
+        <TodoDetail id={id} />
 
         <Link href="/">
           <Button className="w-full">돌아가기</Button>
         </Link>
       </div>
     </section>
+    </HydrationBoundary>
   );
 };
 
